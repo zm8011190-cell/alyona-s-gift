df0bdf3745ba2743db224fe63c726e93a04c9432
unknown <zm8011190@gmail.com>
2026-09-14 06:13:59 +0300

    hmmm

--- Birthday-master/effect.js
+++ Birthday-master/effect.js
@@ -34,12 +34,12 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6').stop();
-			$('#b11').animate({top:260, left: vw-250},500);
-			$('#b22').animate({top:260, left: vw-150},500);
-			$('#b33').animate({top:260, left: vw-50},500);
-			$('#b44').animate({top:260, left: vw+50},500);
-			$('#b55').animate({top:260, left: vw+150},500);
-			$('#b66').animate({top:260, left: vw+250},500);
+			$('#b11').animate({top:260, left: vw-150},500);
+			$('#b22').animate({top:260, left: vw-50},500);
+			$('#b33').animate({top:260, left: vw+50},500);
+			$('#b44').animate({top:260, left: vw+150},500);
+			$('#b55').animate({top:260, left: vw+250},500);
+			$('#b66').animate({top:260, left: vw+350},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -168,12 +168,12 @@ $('document').ready(function(){
 		$('#b4').attr('id','b44')
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
-		$('#b11').animate({top:260, left: vw-250},500);
-		$('#b22').animate({top:260, left: vw-150},500);
-		$('#b33').animate({top:260, left: vw-50},500);
-		$('#b44').animate({top:260, left: vw+50},500);
-		$('#b55').animate({top:260, left: vw+150},500);
-		$('#b66').animate({top:260, left: vw+250},500);
+		$('#b11').animate({top:260, left: vw-150},500);
+		$('#b22').animate({top:260, left: vw-50},500);
+		$('#b33').animate({top:260, left: vw+50},500);
+		$('#b44').animate({top:260, left: vw+150},500);
+		$('#b55').animate({top:260, left: vw+250},500);
+		$('#b66').animate({top:260, left: vw+350},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
